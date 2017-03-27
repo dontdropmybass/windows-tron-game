e52917525fe10a15c7b9baa1313c6afe39b4068c
Alex Cochrane <alex.g.cochrane@gmail.com>
2017-03-27 10:24:54 -0300

    initial commit

---
+++ TronGame/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TronGame
+{
+    public partial class Form1 : Form
+    {
+        bool playing = false;
+        
+        TronBike redBike;
+        TronBike blueBike;
+
+        string winner = "";
+
+        //text font
+        Font font;
+        SolidBrush textBrush;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (playing)
+            {
+                switch (e.KeyCode)
+                {
+                    // Red Bike Controls
+                    case Keys.W:
+                        redBike.direction = DIRECTION.UP;
+                        break;
+                    case Keys.A:
+                        redBike.direction = DIRECTION.LEFT;
+                        break;
+                    case Keys.S:
+                        redBike.direction = DIRECTION.DOWN;
+                        break;
+                    case Keys.D:
+                        redBike.direction = DIRECTION.RIGHT;
+                        break;
+
+                    // Blue Bike Controls
+                    case Keys.Up:
+                        blueBike.direction = DIRECTION.UP;
+                        break;
+                    case Keys.Left:
+                        blueBike.direction = DIRECTION.LEFT;
+                        break;
+                    case Keys.Down:
+                        blueBike.direction = DIRECTION.DOWN;
+                        break;
+                    case Keys.Right:
+                        blueBike.direction = DIRECTION.RIGHT;
+                        break;
+                }
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Return)
+                {
+                    redBike = new TronBike(Width / 10, Height / 2, DIRECTION.RIGHT, Color.Red, Color.OrangeRed);
+                    blueBike = new TronBike((Width / 10) * 9, Height / 2, DIRECTION.LEFT, Color.Blue, Color.LightBlue);
+                    playing = true;
+                    timer1.Start();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+
+            font = new Font("Comic Sans MS", 18);
+            textBrush = new SolidBrush(Color.White);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private Collision CheckCollision()
+        {
+            foreach (Position position in redBike.path)
+            {
+                if (position.X == blueBike.position.X && position.Y == blueBike.position.Y)
+                {
+                    return Collision.Blue;
+                }
+                else if (position.X == redBike.position.X && position.Y == redBike.position.Y)
+                {
+                    return Collision.Red;
+                }
+            }
+            foreach (Position position in blueBike.path)
+            {
+                if (position.X == blueBike.position.X && position.Y == blueBike.position.Y)
+                {
+                    return Collision.Blue;
+                }
+                else if (position.X == redBike.position.X && position.Y == redBike.position.Y)
+                {
+                    return Collision.Red;
+                }
+            }
+            return Collision.None;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (playing)
+            {
+                redBike.Paint(e.Graphics);
+                blueBike.Paint(e.Graphics);
+                Collision collision = CheckCollision();
+                switch (collision)
+                {
+                    case Collision.Red:
+                        winner = "Red Bike";
+                        playing = false;
+                        break;
+                    case Collision.Blue:
+                        winner = "Blue Bike";
+                        playing = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                e.Graphics.DrawString
+                (
+                    "Red bike uses WASD, blue bike uses arrow keys. Press enter to start.",
+                    font,
+                    textBrush,
+                    new Point(0, 0)
+                );
+                timer1.Stop();
+            }
+        }
+    }
+
+    enum Collision { None, Red, Blue }
+}
---
+++ TronGame/TronBike.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TronGame
+{
+    public class TronBike
+    {
+        public List<Position> path { get; }
+        public Position position { get; }
+        private DIRECTION _direction;
+        public DIRECTION direction
+        {
+            get
+            {
+                return _direction;
+            }
+            set
+            {
+                switch (_direction)
+                {
+                    case DIRECTION.UP:
+                        if (value == DIRECTION.LEFT || value == DIRECTION.RIGHT)
+                        {
+                            _direction = value;
+                        }
+                        break;
+                    case DIRECTION.DOWN:
+                        if (value == DIRECTION.LEFT || value == DIRECTION.RIGHT)
+                        {
+                            _direction = value;
+                        }
+                        break;
+                    case DIRECTION.LEFT:
+                        if (value == DIRECTION.UP || value == DIRECTION.DOWN)
+                        {
+                            _direction = value;
+                        }
+                        break;
+                    case DIRECTION.RIGHT:
+                        if (value == DIRECTION.UP || value == DIRECTION.DOWN)
+                        {
+                            _direction = value;
+                        }
+                        break;
+                }
+            }
+        }
+        public Color color { get; }
+        public Color trailColor { get; }
+
+        public TronBike(int x, int y, DIRECTION direction, Color color, Color trailColor)
+        {
+            position = new Position(x, y);
+            this.direction = direction;
+            this.color = color;
+            this.trailColor = trailColor;
+            path = new List<Position>();
+        }
+
+        public void Paint(Graphics graphics)
+        {
+            path.Add(new Position(position));
+            SolidBrush brush = new SolidBrush(color);
+            switch (direction)
+            {
+                case DIRECTION.UP:
+                    position.Y--;
+                    break;
+                case DIRECTION.DOWN:
+                    position.Y++;
+                    break;
+                case DIRECTION.LEFT:
+                    position.X--;
+                    break;
+                case DIRECTION.RIGHT:
+                    position.X++;
+                    break;
+            }
+            graphics.FillRectangle(brush, position.X, position.Y, 1, 1);
+            brush.Color = trailColor;
+            foreach (Position pos in path)
+            {
+                graphics.FillRectangle(brush, pos.X, pos.Y, 1, 1);
+            }
+        }
+    }
+
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Position(Position position)
+        {
+            X = position.X;
+            Y = position.Y;
+        }
+    }
+
+    public enum DIRECTION { UP, LEFT, DOWN, RIGHT }
+}
